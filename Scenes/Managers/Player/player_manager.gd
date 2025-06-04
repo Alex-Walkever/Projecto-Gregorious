e737671b9e0e1c697a8544da3a43045c11460ae9
Walkever <alejandro.rodriguez.navarro92@gmail.com>
2025-06-04 09:50:34 -0600

    adding changes for camera, weapons and enemies

--- Scenes/Managers/Player/player_manager.gd
+++ Scenes/Managers/Player/player_manager.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var player_hp: int = 100
 var player_weapon_resource: Array[Resource]
+var amount_enemies_in_room: int = 2
 
 func _ready() -> void:
 	player_weapon_resource.append(load("res://Scenes/Weapons/Unarmeds/Punch.tscn"))
--- Scripts/weapon.gd
+++ Scripts/weapon.gd
@@ -9,6 +9,7 @@ enum WeaponType{
 @export var damage: int = 1
 @export var type: WeaponType = WeaponType.unarmed
 @export var max_combo: int = 1
+@export var knockback_strength: float = 150.0
 
 var combo_count: int = 0
 var is_attacking: bool = false
@@ -23,3 +24,11 @@ func secundary_action():
 
 func set_direction(direction: float):
 	last_direction = direction
+
+func deal_damage(body: Node2D) -> void:
+	if body is Enemy:
+		var distance_to_enemy: Vector2 = body.global_position - global_position
+		var direction: Vector2 = distance_to_enemy.normalized()
+		body.velocity += direction * knockback_strength
+	
+	body.take_damage(damage)
